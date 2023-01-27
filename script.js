98a1fb603312372cc96cc03a4f38fdbc6e8a4a4a
arjundevlop <109807311+arjundevlop@users.noreply.github.com>
2023-01-27 21:09:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // your code here
+
+	document.getElementById("button").addEventListener("click", function(){
+		var inputNameValue = document.getElementById("name").value;
+		var inputYearValue = document.getElementById("year").value;
+		document.getElementById("url").innerHTML = "https://localhost:8080/?name="+ inputNameValue+"&year="+inputYearValue;
+	});
